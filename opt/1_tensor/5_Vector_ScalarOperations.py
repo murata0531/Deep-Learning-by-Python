db60efb2a6b5bec67fe3331467b81b86dad26477
murata0531 <coless115@gmail.com>
2021-10-12 23:35:40 +0900

    scalar

--- opt/1_tensor/5_Vector_ScalarOperations.py
+++ opt/1_tensor/5_Vector_ScalarOperations.py
@@ -2,7 +2,7 @@ import numpy as np
 
 x = np.array([]1,3,4,5],dtype = float)
 
-# 0階テンソル
+# 0階テンソル(スカラー)
 y = np.array(10)
 
 # ベクトルに0階テンソルを加算
@@ -10,3 +10,6 @@ print(x + y)
 
 # スカラーを加算
 print(x + 10)
+
+# 乗算
+print(x *10)
